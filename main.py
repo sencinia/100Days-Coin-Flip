ebaf2d365585009e6a90b38970f4be7f8de40eec
sencinia <91294669+sencinia@users.noreply.github.com>
2022-04-25 20:02:28 -0700

    Created main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+face = random.randint(0,1)
+
+if face == 0:
+  print("Heads!")
+else:
+  print("Tails!")
+
